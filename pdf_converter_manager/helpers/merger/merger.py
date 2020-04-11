44b9609c213aca971f1fbe08aaeb7a5896cbe5d7
Matteo Dileo <dileo.matteo@gmail.com>
2020-04-11 19:08:15 +0200

    IMG-TO-PDF Fix overwrite pdf files issue

--- pdf_converter_manager/helpers/merger/merger.py
+++ pdf_converter_manager/helpers/merger/merger.py
@@ -1,12 +1,10 @@
 from PyPDF2 import PdfFileMerger
 from error_handler.error_handler import log_handler
-from utilities import utilities
 
 
-def merge_pdf(root_path: str, pdf_to_be_merged: list, compression: bool) -> None:
+def merge_pdf(pdf_to_be_merged: list, compression: bool) -> None:
     """
     Method that merges each temporary pdf file created from the requested pictures.
-    :param root_path: absolute path of the temporary folder.
     :param pdf_to_be_merged: list that contains the names of each temporary pdf.
     :param compression: flag for the compression mode.
     :return: None
@@ -23,5 +21,3 @@ def merge_pdf(root_path: str, pdf_to_be_merged: list, compression: bool) -> None
     except Exception as e1:  # For any unknown exception so far
         with open("pdf_converter_log.txt", "a") as f:
             print(log_handler(e1, action="merging mode"), file=f)
-    finally:
-        utilities.remove_tmp_folder(root_path)  # Deletion of all the temporary files that are required no more
